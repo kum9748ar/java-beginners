8dec7ad5c9d927e36b26d5a38b2f5dc92a6f7d12
K <67071462+kum9748ar@users.noreply.github.com>
2022-08-09 21:28:05 +0530

    Delete Java_Basics directory

--- Java_Basics/FruitNames.java
+++
@@ -1,7 +0,0 @@
-package Java_Basics;
-
-public class FruitNames {
-    public static void main(String[] args) {
-        System.out.println("Name of five fruits: \n Apple \n Mango \n Lichi \n Grapes \n Guava");
-    }
-}
